c226fd7d3380075373aa1d650fd7652138ae1512
Jim Ray <jimiray@gmail.com>
2014-02-27 14:56:12 -0700

    use 64 bit driver

--- scripts/start_linux_grid.sh
+++ scripts/start_linux_grid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DISPLAY=":99"
-/usr/bin/java  -jar ../jars/selenium-server-standalone-2.39.0.jar -role webdriver -hub http://0.0.0.0:4444/grid/register -host 127.0.0.1 -port 5445  -Dwebdriver.chrome.driver=../drivers/linux/chromedriver >&  ../logs/selenium_grid.log &
+/usr/bin/java  -jar ../jars/selenium-server-standalone-2.39.0.jar -role webdriver -hub http://0.0.0.0:4444/grid/register -host 127.0.0.1 -port 5445  -Dwebdriver.chrome.driver=../drivers/linux/chromedriver64 >&  ../logs/selenium_grid.log &
