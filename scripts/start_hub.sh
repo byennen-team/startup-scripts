9d001f3762b4805b80a20358dcc7266f35cb37f6
Jim Ray <jimiray@gmail.com>
2014-06-12 08:13:16 -0600

    update selenium rename hub

--- scripts/start_hub.sh
+++ scripts/start_hub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DISPLAY=":99"
-/usr/bin/java -jar "../jars/selenium-standalone.jar" -role hub >& ../logs/selenium_hub.log &
+/usr/bin/java -jar "../jars/selenium-standalone-hub.jar" -role hub >& ../logs/selenium_hub.log &
