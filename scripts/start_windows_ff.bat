7983787c2ffd7e2cb90c1db200d15b63d1eee23f
Jim Ray <jimiray@gmail.com>
2014-06-12 08:17:41 -0600

    udpate selenium jar and driver for IE

--- scripts/start_windows_ff.bat
+++ scripts/start_windows_ff.bat
@@ -1,1 +1,1 @@
-java -jar ..\jars\selenium-server-standalone-2.39.0.jar -role webdriver -hub http://172.31.39.168:4444/grid/register -host 172.31.17.167 -port 5444 -Dwebdriver.ie.driver=..\drivers\windows\IEDriverServer64.exe -Dwebdriver.chrome.driver=..\driver\windows\chromedriver.exe
+java -jar ..\jars\selenium-server-standalone-2.42.2.jar -role webdriver -hub http://172.31.39.168:4444/grid/register -host 172.31.17.167 -port 5444 -browser browserName="firefox",maxInstances=10
