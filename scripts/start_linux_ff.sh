a0b334bf905e9c02eee8bfe4c220043713d3b35c
Ubuntu <ubuntu@ip-172-31-23-206.us-west-2.compute.internal>
2014-06-12 22:04:27 +0000

    add new scripts for ff and chrome

---
+++ scripts/start_linux_ff.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DISPLAY=":99"
+/usr/bin/java  -jar ../jars/selenium-server-standalone-2.42.2.jar -role webdriver -debug -hub http://172.31.39.168:4444/grid/register -host 172.31.23.206 -port 5445 >&  ../logs/selenium_grid.log &
